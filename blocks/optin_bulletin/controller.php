42e13fdff7f7b375c29b60ac17f537a093237613
William Bargent <w.bargent@canterburycollege.ac.uk>
2016-09-09 11:38:03 +0100

    Add Base Block Template

---
+++ blocks/optin_bulletin/controller.php
@@ -0,0 +1,28 @@
+<?php  
+
+namespace Concrete\Package\RemsCourse\Block\OptInBulletin;
+use \Concrete\Core\Block\BlockController, BlockType, Loader;
+
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class Controller extends BlockController {
+		
+    protected $btTable = 'btOptInBulletin';
+    protected $btInterfaceWidth = "400";
+    protected $btInterfaceHeight = "300";
+    protected $btWrapperClass = 'ccm-ui';
+    protected $btCacheBlockRecord = true;
+    protected $btCacheBlockOutput = true;
+    protected $btCacheBlockOutputOnPost = true;
+    protected $btCacheBlockOutputForRegisteredUsers = true;
+		
+    public function getBlockTypeName() {
+        return t("Opt-in Bulletin");
+    }
+    public function getBlockTypeDescription() {
+        return t("Subscribe to bulletin Email notifications.");
+    }
+    public function registerAssets(){
+   	$this->requireAsset('javascript', 'jquery');
+    }
+}
---
+++ controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Concrete\Package\OptInBulletin;
+use BlockType, Package, Loader;
+
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class Controller extends Package {
+
+    protected $pkgHandle = 'optin_bulletin';
+    protected $appVersionRequired = '5.7.0';
+    protected $pkgVersion = '1.0';
+
+    public function getPackageName() {
+        return t("Opt-in Bulletin");
+    }
+    public function getPackageDescription() {
+        return t("Subscribe to bulletin Email notifications.");
+    }
+
+    public function install() {
+        $pkg = parent::install();
+        BlockType::installBlockTypeFromPackage('optin_bulletin', $pkg); 
+    }
+    public function uninstall(){
+        parent::uninstall();
+        $db = Loader::db();
+        $db->Execute('DROP TABLE IF EXISTS btOptInBulletin');
+    }
+}
